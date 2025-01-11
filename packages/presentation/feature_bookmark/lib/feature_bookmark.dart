dce58f3db864f8fbd7493296c2a6543b02e63c2e
dhik-a <anhafad@gmail.com>
2025-01-11 21:38:54 +0700

    setup feature_bookmark

---
+++ packages/presentation/feature_bookmark/lib/feature_bookmark.dart
@@ -0,0 +1,7 @@
+library feature_bookmark;
+
+export 'feature_bookmark.dart';
+export 'src/config/di.config.dart';
+export 'src/config/feature_bookmark_config.dart';
+export 'src/config/feature_bookmark_router.dart';
+export 'src/config/feature_bookmark_router.gr.dart';
---
+++ packages/presentation/feature_bookmark/lib/src/page/bookmark_page.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class FeedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("Not implemented yet! Sorry!"),
+        ),
+      ),
+    );
+  }
+}
