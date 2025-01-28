0f5551ca642cfaeca0c3a369b414123f3eb7094a
dhik-a <anhafad@gmail.com>
2025-01-28 22:40:16 +0700

    implement repository for post and bloc for feed

---
+++ packages/data/data_post/lib/src/data_source/post_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:domain_post/domain_post.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class PostDataSource {
+  Future<List<Post>> getPosts();
+}
+
+@LazySingleton(as: PostDataSource)
+class PostDataSourceImpl implements PostDataSource {
+  @override
+  Future<List<Post>> getPosts() async {
+    return List.generate(
+      10,
+      (index) => Post(
+        id: '$index',
+        title: 'Post $index',
+        body: 'Post body $index',
+      ),
+    );
+  }
+}
---
+++ packages/presentation/feature_feed/lib/src/bloc/feed_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:domain_post/domain_post.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'feed_event.dart';
+
+part 'feed_state.dart';
+
+part 'feed_bloc.freezed.dart';
